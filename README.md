14d9674772f7d4a7616d00d61f28c69faafec718
PavelWol <59643432+PavelWol@users.noreply.github.com>
2021-09-20 15:18:04 +0200

    init nuxt

---
+++ README.md
@@ -0,0 +1,5 @@
+# Studna website
+Website for studna project.
+
+## Design (Figma)
+Design is provided in this figma file https://www.figma.com/file/C8AAvOuAHMtt3KVPqXm24U/Studna-Web?node-id=0%3A1
---
+++ client/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+
+body {
+  margin: 0;
+}
+
+h1, h2, h3 {
+  margin-top: 0px;
+  margin-bottom: 0px;
+}
+
+p {
+  margin: 0;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 29px;
+}
+
+</style>
