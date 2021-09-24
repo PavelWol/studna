395e2b61a93ba374f5873e3732a3bade90c5ba30
PavelWol <59643432+PavelWol@users.noreply.github.com>
2021-09-24 22:27:21 +0200

    title

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'client',
+    title: 'Studna živé vody',
     htmlAttrs: {
       lang: 'en'
     },
